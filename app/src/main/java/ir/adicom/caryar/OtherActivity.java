643ee9c1f921a5728c9b7dbc8fb7693f23f6d909
Adicom.ir <adicom.ir@gmail.com>
2018-01-02 23:54:46 +0330

    Add reminder

--- app/src/main/java/ir/adicom/caryar/OtherActivity.java
+++ app/src/main/java/ir/adicom/caryar/OtherActivity.java
@@ -3,11 +3,22 @@ package ir.adicom.caryar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.cengalabs.flatui.FlatUI;
+
+import ir.adicom.caryar.other.OtherFragment;
+
 public class OtherActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FlatUI.initDefaultValues(this);
+        FlatUI.setDefaultTheme(FlatUI.DEEP);
         setContentView(R.layout.activity_other);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentParentViewGroup, new OtherFragment())
+                .commit();
     }
 }
