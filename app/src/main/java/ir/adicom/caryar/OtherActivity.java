c111c70cb8fec43f8eaef3908f4ca960b12a9a15
Adicom.ir <adicom.ir@gmail.com>
2018-01-06 23:42:34 +0330

    Add TabMaterial

--- app/src/main/java/ir/adicom/caryar/OtherActivity.java
+++ app/src/main/java/ir/adicom/caryar/OtherActivity.java
@@ -1,13 +1,26 @@
 package ir.adicom.caryar;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import com.cengalabs.flatui.FlatUI;
 
+import ir.adicom.caryar.other.InsuranceTabFragment;
 import ir.adicom.caryar.other.OtherFragment;
+import ir.adicom.caryar.other.ReminderTabFragment;
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
 
-public class OtherActivity extends FragmentActivity {
+public class OtherActivity extends FragmentActivity implements MaterialTabListener {
+
+    MaterialTabHost tabHost;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +29,66 @@ public class OtherActivity extends FragmentActivity {
         FlatUI.setDefaultTheme(FlatUI.DEEP);
         setContentView(R.layout.activity_other);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.fragmentParentViewGroup, new OtherFragment())
-                .commit();
+        tabHost = (MaterialTabHost) this.findViewById(R.id.tabHost);
+        pager = (ViewPager) this.findViewById(R.id.pager );
+
+        // init view pager
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // when user do a swipe the selected tab change
+                tabHost.setSelectedNavigationItem(position);
+            }
+        });
+
+        // insert all tabs from pagerAdapter data
+        for (int i = 0; i < adapter.getCount(); i++) {
+            tabHost.addTab(
+                    tabHost.newTab()
+                            .setText(adapter.getPageTitle(i))
+                            .setTabListener(this)
+            );
+        }
+
+
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.fragmentParentViewGroup, new OtherFragment())
+//                .commit();
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab tab) {
+        pager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab tab) {}
+
+    @Override
+    public void onTabUnselected(MaterialTab tab) {}
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public Fragment getItem(int num) {
+            if (num == 0) return new InsuranceTabFragment();
+            else return new ReminderTabFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0) return "بیمه";
+            else return "یاداوری";
+        }
     }
 }
