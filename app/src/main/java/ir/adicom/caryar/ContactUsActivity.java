43e3cfa695e5135141ce8115d7c98222987e33f3
Adicom.ir <adicom.ir@gmail.com>
2018-02-18 22:24:33 +0330

    Fix myket bugs

--- app/src/main/java/ir/adicom/caryar/ContactUsActivity.java
+++ app/src/main/java/ir/adicom/caryar/ContactUsActivity.java
@@ -1,9 +1,13 @@
 package ir.adicom.caryar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cengalabs.flatui.FlatUI;
 
@@ -18,5 +22,17 @@ public class ContactUsActivity extends Activity {
 
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "Samim.ttf");
         HelperUI.setFont((ViewGroup) getWindow().getDecorView(), custom_font);
+
+        Button btnCm = (Button) findViewById(R.id.btn_cm);
+        btnCm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url= "myket://comment?id=ir.adicom.caryar";
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
