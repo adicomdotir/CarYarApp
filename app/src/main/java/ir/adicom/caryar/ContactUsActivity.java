13888842ac65f37ec0a19692a11695f53e4f6008
Adicom.ir <adicom.ir@gmail.com>
2018-06-04 16:13:41 +0430

    Merge branch 'master' of https://github.com/adicomdotir/CarYarApp
    # Conflicts:
    #	app/build/intermediates/incremental/mergeDebugResources/compile-file-map.properties
    #	app/build/intermediates/res/resources-debug.ap_

---
+++ app/src/main/java/ir/adicom/caryar/ContactUsActivity.java
@@ -0,0 +1,38 @@
+package ir.adicom.caryar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.cengalabs.flatui.FlatUI;
+
+public class ContactUsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FlatUI.initDefaultValues(this);
+        FlatUI.setDefaultTheme(FlatUI.DARK);
+        setContentView(R.layout.activity_contact_us);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "Samim.ttf");
+        HelperUI.setFont((ViewGroup) getWindow().getDecorView(), custom_font);
+
+        Button btnCm = (Button) findViewById(R.id.btn_cm);
+        btnCm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url= "myket://comment?id=ir.adicom.caryar";
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}
